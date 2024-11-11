7960b50aadeca011b43419f793e2211b03d5ae04
AntonGog171 <antongogol2017@gmail.com>
2024-11-11 22:31:15 +0200

    fixed tests and edited requirements.txt

--- compute/tests/test_compute.py
+++ compute/tests/test_compute.py
@@ -1,71 +1,80 @@
-import unittest
-from compute.models.communication import ComputeRequest
+import pytest
+
 from compute.engine import ComputeEngine
+from compute.models.communication import ComputeRequest
 from compute.rabbitmq_broker import RabbitMQBroker
 from detectors.mocks import MockDetector
 
 
-class TestComputeEngine(unittest.TestCase):
-    def setUp(self):
-        self.mock_detector = MockDetector(detector_name="mock_detector", labels=["label1", "label2"])
+@pytest.fixture
+def setup_engine():
+    mock_detector = MockDetector(detector_name="mock_detector", labels=["label1", "label2"])
 
-        self.mock_broker = RabbitMQBroker(
-            source_queue_name="requests_queue",
-            response_queue_name="responses_queue",
-            rabbitmq_host="localhost",
-            rabbitmq_port=5672
+    mock_broker = RabbitMQBroker(
+        source_queue_name="requests_queue",
+        response_queue_name="responses_queue",
+        rabbitmq_host="localhost",
+        rabbitmq_port=5672
+    )
 
-        )
-
-        self.engine = ComputeEngine(detectors=[self.mock_detector], default_detector=self.mock_detector,
-                                    broker=self.mock_broker)
-
-    def test_bad_detector_request(self):
-        # Create a mock request
-        request = ComputeRequest(
-            request_id="test_request_id",
-            content="test_content",
-            detector_name="mock_detector_non_existant"
-        )
-
-        # Call process_request
-        response = self.engine.process_request(request)
-
-        # Verify the response structure
-        self.assertEqual(response.request_id, "test_request_id")
-        self.assertEqual(response.explanation, "Prediction based on provided content")
-
-    def test_process_request(self):
-        # Create a mock request
-        request = ComputeRequest(
-            request_id="test_request_id",
-            content="test_content",
-            detector_name="mock_detector"
-        )
-
-        # Call process_request
-        response = self.engine.process_request(request)
-
-        # Verify the response structure
-        self.assertEqual(response.request_id, "test_request_id")
-        self.assertEqual(response.explanation, "Prediction based on provided content")
-
-    def test_get_detector_by_name(self):
-        # Check if the detector is returned correctly
-        detector = self.engine.get_detector_by_name("mock_detector")
-        self.assertEqual(detector, self.mock_detector)
-
-        # Check if None is returned when the detector is not found
-        detector = self.engine.get_detector_by_name("non_existent_detector")
-        self.assertEqual(detector, self.engine.default_detector)
-
-    def test_start_and_stop_consuming(self):
-        self.engine.start_consuming()
-        self.engine.stop_consuming()
-
-    def test_close(self):
-        self.engine.close()
+    engine = ComputeEngine(detectors=[mock_detector], default_detector=mock_detector, broker=mock_broker)
+    return engine, mock_detector
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_bad_detector_request(setup_engine):
+    engine, _ = setup_engine
+
+    # Create a mock request
+    request = ComputeRequest(
+        request_id="test_request_id",
+        content="test_content",
+        detector_name="mock_detector_non_existant"
+    )
+
+    # Call process_request
+    response = engine.process_request(request)
+
+    # Verify the response structure
+    assert response.request_id == "test_request_id"
+    assert response.explanation == "Prediction based on provided content"
+
+
+def test_process_request(setup_engine):
+    engine, _ = setup_engine
+
+    # Create a mock request
+    request = ComputeRequest(
+        request_id="test_request_id",
+        content="test_content",
+        detector_name="mock_detector"
+    )
+
+    # Call process_request
+    response = engine.process_request(request)
+
+    # Verify the response structure
+    assert response.request_id == "test_request_id"
+    assert response.explanation == "Prediction based on provided content"
+
+
+def test_get_detector_by_name(setup_engine):
+    engine, mock_detector = setup_engine
+
+    # Check if the detector is returned correctly
+    detector = engine.get_detector_by_name("mock_detector")
+    assert detector == mock_detector
+
+    # Check if None is returned when the detector is not found
+    detector = engine.get_detector_by_name("non_existent_detector")
+    assert detector == engine.default_detector
+
+
+def test_start_and_stop_consuming(setup_engine):
+    engine, _ = setup_engine
+    engine.start_consuming()
+    engine.stop_consuming()
+
+
+def test_close(setup_engine):
+    engine, _ = setup_engine
+    engine.close()
